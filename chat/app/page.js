0a38a0a93c9770eec032e4cb13ff4185c38baa49
Chris.T.Kuo <kuoaws@gmail.com>
2025-01-07 00:57:40 +0800

    css

--- chat/app/page.js
+++ chat/app/page.js
@@ -3,93 +3,42 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <>
+      <header className="page-header">
+        <a href="">
+          <img src="/images/logo.png" />
+        </a>
+        <ul className="header-nav">
+          <li><a href="">國內校區</a></li>
+          <li><a href="">美洲校區</a></li>
+          <li><a href="">歐洲校區</a></li>
+          <li><a href="">日本校區</a></li>
+        </ul>
+      </header>
+      <div className="page-content">
+        <div className="content-nav">
+          <div className="item">
+            <img src="/images/item1.png" />
+            <span>my email</span>
+          </div>
+          <div className="item">
+            <img src="/images/item2.png" />
+            <span>my email</span>
+          </div>
+          <div className="item">
+            <img src="/images/item3.png" />
+            <span>my email</span>
+          </div>
+          <div className="item">
+            <img src="/images/item4.png" />
+            <span>my email</span>
+          </div>
+          <div className="item">
+            <img src="/images/item5.png" />
+            <span>my email</span>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
---
+++ chat/app/product/page.js
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+const page = () => {
+  return (
+    <div className={styles.pd}>
+        <p>this is page</p>
+    </div>
+  )
+}
+
+export default page
